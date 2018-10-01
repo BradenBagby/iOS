7f4b0d115ad630dee52e7b9905c9cdba352dd869
grbcp5 <grbcp5@mst.edu>
2018-09-30 20:07:11 -0500

    Initial commit.

---
+++ PaceRides/PaceRides/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  PaceRides
+//
+//  Created by Grant Broadwater on 9/30/18.
+//  Copyright © 2018 PaceRides. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
