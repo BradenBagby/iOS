cab49856499e67fd3ac6d3159dfdced6f39ae0f0
grbcp5 <grbcp5@mst.edu>
2018-10-22 14:20:50 -0500

    Ride tab bare minimum

--- PaceRides/PaceRides/ViewControllers/Public/Tabs/RideViewController.swift
+++ PaceRides/PaceRides/ViewControllers/Public/Tabs/RideViewController.swift
@@ -10,8 +10,41 @@ import UIKit
 
 class RideViewController: PaceTabViewController {
     
+    @IBOutlet weak var noRideView: UIView!
+    @IBOutlet weak var primaryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad(self.view)
+        
+        self.newUserData()
+        UserModel.notificationCenter.addObserver(
+            forName: .NewPaceUserData,
+            object: nil,
+            queue: OperationQueue.main,
+            using: self.newUserData
+        )
+    }
+    
+    func newUserData(_: Notification? = nil) {
+        
+        guard let paceUser = UserModel.sharedInstance(), let userRide = paceUser.ride else {
+            self.noRideView.isHidden = false
+            return
+        }
+        
+        userRide.subscribe(using: self.newRideData)
+    }
+    
+    
+    func newRideData(_: Notification? = nil) {
+        
+        guard let paceUser = UserModel.sharedInstance(), let userRide = paceUser.ride else {
+            self.noRideView.isHidden = false
+            return
+        }
+        self.noRideView.isHidden = true
+        
+        self.primaryLabel.text = userRide.eventTitle ?? "Error"
     }
     
 }
